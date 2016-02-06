83976635171775e6f4671d485e8bf32b1ded8cfd
Nacho Morinigo <morinacho@gmail.com>
2016-02-06 18:26:58 -0300

    Agregado de pluggins. LaravelCollective/html -> Para Formularios | LaraCasts/flash -> Para mensajes de alertas | Cviebrock/sluggable -> Para el manejo de url.

--- config/app.php
+++ config/app.php
@@ -156,6 +156,13 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         *  Personal Service Providers
+         */
+        Collective\Html\HtmlServiceProvider::class,
+        Cviebrock\EloquentSluggable\SluggableServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +208,12 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+    /*
+     *  Personal Aliases
+     */
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
 
